5d5563e96cf48b62c797d3558016b3e9dba4d9c0
Juan Diego Gonzalez <njuand2g@gmail.com>
2014-11-23 21:35:50 -0500

    Explica como for nos facilita hacer ciclos

---
+++ pruebafor.py
@@ -0,0 +1,38 @@
+
+for contador in [0,1,2,3,4,5,6,7]:
+	print contador
+
+print "Fin prueba basica"
+
+qwer=0+1+2+3+4+5+6+7+8+9
+
+
+print qwer
+
+suma=0
+
+for i in range(1,11):
+	suma=suma+i 
+print "resultado suma"
+print suma
+
+suma=0
+
+for i in range(1,11):
+	if i%2==0:
+		suma=suma+i
+	
+print "resultado suma de los pares es"
+print suma
+	
+suma=0
+
+for i in range(1,11):
+	if i%2!=0:
+		suma=suma+i
+	
+print "resultado suma de los impares es"
+print suma
+
+
+	
