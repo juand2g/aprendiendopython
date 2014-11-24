72f1d161776500dac7dcbbe16dc58d82f82930cf
Juan Diego Gonzalez <njuand2g@gmail.com>
2014-11-23 21:35:20 -0500

    Codigo de ejemplo variables y arreglos

---
+++ ejemplos.py
@@ -0,0 +1,22 @@
+print "juand2g"
+a=50
+print a 
+a=30+25
+print a
+
+
+
+
+
+
+
+
+
+llavepublica=[1,0,1,0,0,1,0,1,0,1]
+print llavepublica
+print llavepublica[0]
+print llavepublica[1]
+print llavepublica[2]
+
+print "---------------------"
+primero=72
